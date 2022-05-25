1ed77777bf94ed942e2d81e2cc06d839c75d37ee
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-05-25 15:07:16 +0200

    webhook

--- src/main/java/fw/partnershipservice/feign/PaymentRestConsumer.java
+++ src/main/java/fw/partnershipservice/feign/PaymentRestConsumer.java
@@ -2,11 +2,13 @@ package fw.partnershipservice.feign;
 
 import fw.partnershipservice.model.CheckoutPayment;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name="fw-payment-service", url="${app.services.payment.url}")
 public interface PaymentRestConsumer {
 
     @PostMapping("/api/payment")
-    void payPartnership(Long partnershipId, CheckoutPayment checkoutPayment);
+    void payPartnership(@PathVariable("partnershipId") Long partnershipId, @RequestBody CheckoutPayment checkoutPayment);
 }
