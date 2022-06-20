7648e37272cd13f0ff241a11ee0ca7581cf4c387
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-06-20 20:12:33 +0200

    host heruku

--- src/main/java/fw/partnershipservice/PartnershipServiceApplication.java
+++ src/main/java/fw/partnershipservice/PartnershipServiceApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class PartnershipServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PartnershipServiceApplication.class, args);
 	}
